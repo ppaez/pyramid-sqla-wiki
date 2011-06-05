0e8230285b12dc6d025cd43b2aacc24af7149909
Patricio Paez <pp@pp.com.mx>
2011-06-05 08:13:57 -0500

    Defining the domain model

--- tutorial/models.py
+++ tutorial/models.py
@@ -2,7 +2,7 @@ import transaction
 
 from sqlalchemy import Column
 from sqlalchemy import Integer
-from sqlalchemy import Unicode
+from sqlalchemy import Text
 
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.declarative import declarative_base
@@ -15,28 +15,26 @@ from zope.sqlalchemy import ZopeTransactionExtension
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
-class MyModel(Base):
-    __tablename__ = 'models'
+class Page(Base):
+    """ The SQLAlchemy declarative model class for a Page object. """
+    __tablename__ = 'pages'
     id = Column(Integer, primary_key=True)
-    name = Column(Unicode(255), unique=True)
-    value = Column(Integer)
+    name = Column(Text, unique=True)
+    data = Column(Text)
 
-    def __init__(self, name, value):
-        self.name = name
-        self.value = value
+    def __init__(self, name, data):
+       self.name = name
+       self.data = data
 
-def populate():
-    session = DBSession()
-    model = MyModel(name=u'root', value=55)
-    session.add(model)
-    session.flush()
-    transaction.commit()
-    
 def initialize_sql(engine):
     DBSession.configure(bind=engine)
     Base.metadata.bind = engine
     Base.metadata.create_all(engine)
     try:
-        populate()
+        session = DBSession()
+        page = Page('FrontPage', 'initial data')
+        session.add(page)
+        transaction.commit()
     except IntegrityError:
-        DBSession.rollback()
+        # already created
+        pass
